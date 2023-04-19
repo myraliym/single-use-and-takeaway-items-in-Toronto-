1a165a6e0c627711bb245f2b161037f050230d41
myraliym <myraliym@gmail.com>
2023-04-19 02:34:14 -0400

    graph generate for plastic containers

--- scripts/datacleaning_single-use.R
+++ scripts/datacleaning_single-use.R
@@ -15,21 +15,36 @@ library(haven)
 library(readxl)
 library(ggplot2)
 library(reshape2)
-
+library(writexl)
 ## input data
 raw_data<-read_xlsx(here::here("inputs/data/Fall 2018.xlsx")) 
 
 # extract the income and race columns
 single_use_data <- raw_data[, c("2.1. Disposable Hot To-Go Cups, such as coffee cups", 
                                 "2.2. Disposable Cold To-Go Cups, such as pop cups",
-                                "2.3. Plastic Bags", "2.4. Paper Bags", 
+                                "2.3. Plastic Bags", 
+                                "2.4. Paper Bags", 
                                 "2.5. Black Plastic Containers",
                                 "2.6. White, Clear, or any colour Plastic Containers",
                                 "2.7. Disposable Cutlery",
-                                "2.8. Plastic Straws", "2.9. Styrofoam Containers")]
+                                "2.8. Plastic Straws", 
+                                "2.9. Styrofoam Containers")]
+# Rename the columns
+single_use_data <- single_use_data %>%
+  rename(
+    "Hot To-Go Cups" = `2.1. Disposable Hot To-Go Cups, such as coffee cups`,
+    "Cold To-Go Cups" = `2.2. Disposable Cold To-Go Cups, such as pop cups`,
+    "Plastic Bags" = `2.3. Plastic Bags`,
+    "Paper Bags" = `2.4. Paper Bags`,
+    "Black Plastic Containers" = `2.5. Black Plastic Containers`,
+    "Other Colour Plastic Containers" = `2.6. White, Clear, or any colour Plastic Containers`,
+    "Disposable Cutlery" = `2.7. Disposable Cutlery`,
+    "Plastic Straws" = `2.8. Plastic Straws`,
+    "Styrofoam Containers" = `2.9. Styrofoam Containers`
+  )
 
 # Remove rows with any 'n/a' or NA values
 single_use_data_clean<- na.omit(single_use_data)
 
 # save the data frame as a csv file
-write.csv(single_use_data_long, file = here::here("outputs/data/single_use_data.csv"), row.names = TRUE)
+write_xlsx(single_use_data_clean, here::here("outputs/data/single_use_data.xlsx"))
